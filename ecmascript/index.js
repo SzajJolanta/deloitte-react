2a46f318d9b6b7a379bf54e76f7c53b9ef3349ae
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-04-03 23:01:06 +0200

    task/ecmascript solution

--- ecmascript/index.js
+++ ecmascript/index.js
@@ -1,6 +1,26 @@
-function createTeamMemberDetails(teamMember) {
-    const name = teamMember.name;
-    const age = teamMember.age;
+// Zadanie 2
 
-    return name + " ma " + age + " lat";
-} 
+const team = document.querySelectorAll('#team li');
+
+const teamArr = Array.from(team);
+
+const mappedTeam = teamArr
+    .map(member => ({
+        name: member.innerText,
+        age: parseInt(member.dataset.age, 10),
+    }));
+
+mappedTeam
+    .filter(member => member.age > 25 && member.age < 35);
+
+const findOldestPerson = members => {
+    const maxAge = Math.max(...members.map(m => m.age));
+
+    return members.find(m => m.age === maxAge);
+}
+
+const sumAgeOfTeamMembers = members => members.reduce((prevSum, currentMember) => prevSum + currentMember.age, 0);
+
+// Zadanie 3
+
+const createTeamMemberDetails = ({ name, age }) => `${name} ma ${age} lat`;
