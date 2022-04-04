ec17ad0d2640a7724b4ad17b2e2deddaa2878eda
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-04-05 00:23:42 +0200

    solution task 1

--- effects/src/components/Counter/Counter.tsx
+++ effects/src/components/Counter/Counter.tsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { convertTime } from "../../utils/convertTime";
 
 const Counter = () => {
     const [time, setTime] = useState(0);
+    const [message, setMessage] = useState('');
     
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime((t) => t + 1);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        if (time === 10) {
+            setMessage('Wow! Minęło już 10 sekund');
+        }
+        if (time === 20) {
+            setMessage('Niesamowite! To jest wynik!');
+        }
+    }, [time]);
 
     return <div>
-        {time}
+        {convertTime(time)}
+        {message && <p>{message}</p>}
     </div>
 }
 
