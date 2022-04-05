9c495968ff688bd043dedf16dba1ca7948d901d3
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-04-05 12:20:54 +0200

    translations

--- .playground/playground/src/App.tsx
+++ .playground/playground/src/App.tsx
@@ -2,12 +2,14 @@ import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Btn from './components/Btn/Btn';
+import translations from './translations';
 
 function App() {
   const [isVisible, setIsVisible] = useState(true);
 
   return (
     <div className="App">
+      {translations.HOME.WELCOME_MSG}
       <button onClick={() => setIsVisible(!isVisible)}>hide / show btn</button>
       {isVisible && <Btn />}
     </div>
---
+++ .playground/playground/src/translations.ts
@@ -0,0 +1,11 @@
+const translations = {
+    
+    HOME: {
+        WELCOME_MSG: 'Witaj na naszej stronie internetowej.',
+    },
+    COMMON: {
+        HIDE: 'Ukryj'
+    }
+}
+
+export default translations;
