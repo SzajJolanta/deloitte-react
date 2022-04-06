5cb660682add89f74c81f9ca5d881a2f35030f7f
Mateusz Jabłoński <mail@mateuszjablonski.com>
2022-04-06 10:49:24 +0200

    styles

--- router/src/App.tsx
+++ router/src/App.tsx
@@ -5,21 +5,17 @@ import './App.css';
 import ContactPage from './pages/Contact';
 import HomePage from './pages/Home';
 import Article from './pages/Article';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
 
 function App() {
   return (
     <Router>
       <div className="App">
-          <Header />
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/contact' element={<ContactPage />} />
             <Route path='/article/:id' element={<Article />} />
           </Routes>
       </div>
-      <Footer />
     </Router>
   );
 }
